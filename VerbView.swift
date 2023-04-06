4154ca313a73f0ceee7aa5be3654c6aaf133f38f
Michael Grabowy <mgrabowy3150@stu.d214.org>
2023-04-06 14:16:55 -0500

    made adjective view

---
+++ VerbView.swift
@@ -0,0 +1,14 @@
+//
+//  VerbView.swift
+//  GitHubPractice2
+//
+//  Created by Michael Grabowy on 4/6/23.
+//
+
+import Foundation
+
+struct VerbView {
+    NavigationLink("AdjectiveView", destination: AdjectiveView())
+    
+    }
+
