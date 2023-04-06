0614c9e61935ba5b67867e72778ba98ff8de76cf
Josh Hartman <jhartman3167@stu.d214.org>
2023-04-06 14:23:47 -0500

    Created MadLib file and struct

---
+++ MadLib.swift
@@ -0,0 +1,16 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Josh Hartman on 4/6/23.
+//
+
+import SwiftUI
+
+struct MadLib {
+    
+    var noun:String
+    var verb:String
+    var adjective:String
+    
+}
