ca218028e79b6ffa26d3ac66102175f435200940
Michael Grabowy <mgrabowy3150@stu.d214.org>
2023-04-06 14:41:27 -0500

    Commit

--- AdjectiveView.swift
+++ AdjectiveView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct AdjectiveView: View {
+    
+    @Binding var enteredWords:MadLib
+    
     var body: some View {
+        
+        TextField("Enter Adjective", text: $enteredWords.adjective)
         NavigationLink("Summary View") {
-            SummaryView()
+            SummaryView(enteredWords)
         }
     }
 }
