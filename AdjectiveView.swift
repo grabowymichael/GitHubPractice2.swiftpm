c362d98a4cae0300bf9d8426e29510005fe6cdda
Josh Hartman <jhartman3167@stu.d214.org>
2023-04-06 14:14:51 -0500

    Created file AdjectiveView Created NavigationLink to SummaryView

---
+++ AdjectiveView.swift
@@ -0,0 +1,17 @@
+//
+//  AdjectiveView.swift
+//  GitHubPractice2
+//
+//  Created by Josh Hartman on 4/6/23.
+//
+
+import SwiftUI
+
+struct AdjectiveView: View {
+    var body: some View {
+        NavigationLink("Summary View") {
+            SummaryView()
+        }
+    }
+}
+
