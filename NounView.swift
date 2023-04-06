4923f0fd915e0f71e918aecdb89c7131e98d46dd
Josh Hartman <jhartman3167@stu.d214.org>
2023-04-06 14:09:05 -0500

    New file NounView Navigation link to VerbView

---
+++ NounView.swift
@@ -0,0 +1,16 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Josh Hartman on 4/6/23.
+//
+
+import SwiftUI
+
+struct NounView: View {
+    var body: some View {
+        NavigationLink("Verb View") {
+            VerbView()
+        }
+    }
+}
